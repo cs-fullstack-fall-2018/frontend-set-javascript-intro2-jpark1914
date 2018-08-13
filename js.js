6e3ba8ec7d48ae413fbc58680c968e25dbade9ad
Jordan Parker <parkerj4321@gmail.com>
2018-08-13 12:11:27 -0500

    Exercises completed

--- js.js
+++ js.js
@@ -1,9 +1,27 @@
-var arrayOfNubers = [30,37,38,45,47];
+var arrayOfNames = ["PJ", "Jordan", "Caleb", "Maddi"];
 
 var stringOfArray = "";
-for(var i = 0; i < arrayOfNubers.length; i++)
+for(var i = 0; i < arrayOfNames.length; i++)
 {
-    stringOfArray += arrayOfNubers[i] + ", ";
+    stringOfArray += arrayOfNames[i] + " ";
 }
 
 document.getElementById("something").textContent= stringOfArray;
+
+var arrayOfNumbers = [12, 34, 32, 55, 7665];
+var newNumbers = "";
+var redoNumbers= "";
+
+for(var x = 0; x<arrayOfNumbers.length; x++)
+{
+    newNumbers += arrayOfNumbers[x] + " ,";
+}
+document.getElementById("header2").innerHTML = newNumbers;
+
+arrayOfNumbers.pop();
+
+for(var c = 0; c<arrayOfNumbers.length; c++){
+    redoNumbers += arrayOfNumbers[c] + ",";
+}
+
+document.getElementById("header3").innerHTML = redoNumbers;
